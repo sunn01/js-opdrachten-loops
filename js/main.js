8bc59ba00610f221f4bdc2272d0291587fd8c80a
sunn01 <302854486@student.rocmondriaan.nl>
2023-09-29 12:55:22 +0200

    js loops opdrachten

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//opdracht 1
+let x = 0;
+while (x <= 9) {
+    console.log('cijfer is:' + x );
+    x++;
+};
+if (x > 9) {
+    console.log('Einde programma')
+};
+
+//opdracht 2
+let y = 2;
+while (y <= 20) {
+    console.log('cijfer is:' + y);
+    y++;  
+};
+if (y > 20) {
+    console.log('Einde programma')
+};
+
+//opdracht 3
+let z = 10; 
+while (z >= 0) {
+    console.log('cijfer is:' + z);
+    z--;
+};
+if (z < 0) {
+    console.log('Einde programma')
+};
+
+//opracht 4
